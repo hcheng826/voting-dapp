6a113dc4a25c43f656f8cfe92ce619e2ddc4d598
hcheng826 <lilioo826@gmail.com>
2021-12-29 09:13:29 +0800

    better table appearance

--- frontend/src/components/ProposalBoard.js
+++ frontend/src/components/ProposalBoard.js
@@ -4,26 +4,29 @@ export function ProposalBoard({ voteOn, proposals }) {
     return (
         <div>
             <h4>Proposal Leaderboard</h4>
-            <table>
-                <tr>
-                    <th>index</th>
-                    <th>content</th>
-                    <th>votes count</th>
-                    <th>vote!</th>
-                </tr>
-                { [...Array(proposals.length).keys()].map((idx) => {
+            <table class="table">
+                <thead>
+                  <tr>
+                      <th>index</th>
+                      <th>content</th>
+                      <th>votes count</th>
+                      <th>vote!</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  { [...Array(proposals.length).keys()].map((idx) => {
                     return (
-                        <tr idx={idx}>
-                            <th>{idx}</th>
-                            <th>{proposals[idx].content}</th>
-                            <th>{proposals[idx].votesCount.toNumber()}</th>
-                            <th>
-                                <button onClick={() => { voteOn(idx) }}>Vote!</button>
-                            </th>
-                        </tr>
+                      <tr idx={idx}>
+                        <th>{idx}</th>
+                        <th>{proposals[idx].content}</th>
+                        <th>{proposals[idx].votesCount.toNumber()}</th>
+                        <th>
+                          <input className="btn btn-primary" type="submit" value="Vote!" onClick={() => { voteOn(idx) }}/>
+                        </th>
+                      </tr>
                     )
-                })}
-
+                  })}
+                </tbody>
             </table>
 
         </div>
